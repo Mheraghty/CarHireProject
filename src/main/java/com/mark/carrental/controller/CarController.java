5845232666330a13c5b065a9599288fe9e83a40c
Mark <mheraghty@overstock.com>
2020-05-26 11:41:21 +0100

    Toyota files

--- src/main/java/com/mark/carrental/controller/CarController.java
+++ src/main/java/com/mark/carrental/controller/CarController.java
@@ -1,4 +1,15 @@
 package com.mark.carrental.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
+    @GetMapping("/car/details")
+    public String getCarDetails() {
+        return "Toyota";
+    }
 }
+
